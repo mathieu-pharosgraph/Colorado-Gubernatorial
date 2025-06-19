c51babe73488a8c5ac1bf4f2c044cf07ab7e27f2
mathieu-pharosgraph <mathieu@pharosgraph.com>
2025-06-19 14:52:09 +0300

    Initial commit: add Streamlit app and data

--- src/14_streamlit-cloud.py
+++ src/14_streamlit-cloud.py
@@ -11,7 +11,7 @@ import pydeck as pdk
 from hashlib import md5
 st.set_page_config(layout="wide")
 import os
-st.write("Deployed files:", os.listdir("data"))
+
 
 # --- Logo and Header ---
 st.image("src/pharos_logo.png", width=180)
